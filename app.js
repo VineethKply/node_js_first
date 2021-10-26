f62061ab9e14e26b7bdce18a26fa8fcc2319982f
Hitlervineeth <vineethmohan6@gmail.com>
2021-10-26 15:04:00 +0530

    add post method

--- app.js
+++ app.js
@@ -6,7 +6,18 @@ var bodyParser = require('body-parser')
 var app=express()
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended : false})
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(urlencodedParser)
+// parse application/json
+app.use(bodyParser.json())
+
+app.post('/', (request,response)=>{
+    console.log('server stated : http://localhost:3000/ post ')
+})
+
+app.get("/",(request,response)=>{
+    console.log('server stated : http://localhost:3000/ get ')
+})
 
 
 app.listen(3000,()=>{
