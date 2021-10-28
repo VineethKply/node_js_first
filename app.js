e3ccc67b5f67eec3721d671b5d1ffb716aae46db
Hitlervineeth <vineethmohan6@gmail.com>
2021-10-28 19:58:02 +0530

    add note class 20

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var {userdetails_model}=require('./modules/userdetails')
 
 
 
-
+mongoose.connect('mongodb+srv://root:root@cluster0.bi1zi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 var app=express()
 
 // parse application/x-www-form-urlencoded
@@ -16,7 +16,20 @@ app.use(bodyParser.json())
 
 app.post('/register', (request,response)=>{
     var usermodelobj = new userdetails_model(request.body);
-    response.send(usermodelobj)
+    usermodelobj.save((error)=>{
+        if(error){
+            response.json({
+                "ResponseStatus": error,
+                "ResponseCode":  000
+            })
+        }else{
+            response.json({
+                "ResponseStatus": "Successfully Registered !",
+                "ResponseCode" : 100
+            })
+        }
+    })
+
 })
 
 app.get("/register",(request,response)=>{
